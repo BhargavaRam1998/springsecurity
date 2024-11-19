80ab9c0691c867d5b08ad0e4b5eff681c6973de7
Bhargava Ram <bhargav@Mac.lan>
2024-11-18 18:36:12 -0600

    code until returning a token but a hard coded value

---
+++ src/main/java/com/telusko/security/service/JWTService.java
@@ -0,0 +1,11 @@
+package com.telusko.security.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+    public String generateToken() {
+        return " token value";
+
+    }
+}
--- src/main/java/com/telusko/security/service/UserService.java
+++ src/main/java/com/telusko/security/service/UserService.java
@@ -4,6 +4,9 @@ package com.telusko.security.service;
 import com.telusko.security.model.Users;
 import com.telusko.security.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,27 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Users register(Users user){
 
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken();
+        else
+            return "fail";
     }
 }
